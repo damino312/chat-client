2e34331d707311c1948cdea5d40671fefd3e733e
Popov Kirill <93064817+damino312@users.noreply.github.com>
2023-09-07 16:21:06 +0700

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ "./src/**/*.jsx"],
+  content: ["./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "wh-bg": "#0c1317",
+        "wh-user": "#182229",
+        "wh-dark-gray": "#202c33",
+        "wh-selected": "#2a3942",
+        "wh-my-message": "#005c4b",
+      },
+    },
   },
   plugins: [],
-}
-
+};
